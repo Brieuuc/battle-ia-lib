c14a99d3380df0261a8022c9faf5aae164713775
Brieuc <brieuc.huot@gmail.com>
2025-01-23 08:17:17 +0100

    Test

--- src/main.c
+++ src/main.c
@@ -1,25 +1,39 @@
 #include "battle_c.h"
-
+#include "stdbool.h"
 #include "stdio.h"
 #include "stdlib.h"
 
+void showPlayerData(BC_PlayerData playerData){
+  printf("ID: %d\n");
+  printf("Position: (%d,%d,%d)\n", playerData.position.x, playerData.position.y, playerData.position.z);
+  printf("Vitesse: (%d,%d,%d)\n", playerData.speed.x, playerData.speed.y, playerData.speed.z);
+  printf("Health: %d\n", playerData.health);
+  printf("Score: %d\n", playerData.score);
+  printf("Armor: %d\n", playerData.armor);
+  printf("Mort: %s\n", playerData.is_dead ? "Yes" : "No");
+  }
+
+
+void radar_ping(BC_Connection *conn) {
+  BC_List *objects = bc_radar_ping(conn);
+  while (objects) {
+    BC_MapObject *object = (BC_MapObject *)bc_ll_value(objects);
+    printf("Objet détecté : ID=%d, Type=%d, Position=(%d, %d, %d)\n",
+           object->id, object->type,
+           object->position.x, object->position.y, object->position.z);
+    objects = bc_ll_next(objects);
+  }
+}
+
+
 int main(int argc, char *argv[]) {
+  // Connexion
+    BC_Connection *conn = bc_connect("5.135.136.236", 8080);
 
-  BC_Connection *conn = bc_connect("5.135.136.236", 8080);
+  // Récupération des données du joueur
+    BC_PlayerData playerData = bc_get_player_data(conn);
+    showPlayerData(playerData);
 
-  bc_get_world_info(conn);
-
-  bc_set_speed(conn, 1.2, 0.4, 0);
-
-  BC_PlayerData data = bc_get_player_data(conn);
-
-  BC_List *list = bc_radar_ping(conn);
-
-  do {
-    BC_MapObject *map_object = (BC_MapObject *)bc_ll_value(list);
-    printf("map_object x = %d, y = %d", map_object->position.x,
-           map_object->position.y);
-
-  } while (((list = bc_ll_next(list)) != NULL));
-  return EXIT_SUCCESS;
+  // Scan radar
+  radar_ping(conn);
 }
